8d0a123aa5b038cd3cf4d20c438865b2f2c49bcf
xiaomeng <sunxm@xingyuanauto.com>
2018-03-20 15:26:17 +0800

    Initial Commit

---
+++ iOSBugTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSBugTest
+//
+//  Created by 小萌 on 2018/3/20.
+//  Copyright © 2018年 sunxm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSBugTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iOSBugTest
+//
+//  Created by 小萌 on 2018/3/20.
+//  Copyright © 2018年 sunxm. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
